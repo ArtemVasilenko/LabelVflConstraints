5fba8258283ba8ca7788e7bfad9c4245e3679666
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-15 21:08:48 +0300

    kudu

--- LabelVflConstraints/ViewController.swift
+++ LabelVflConstraints/ViewController.swift
@@ -4,23 +4,34 @@ import UIKit
 
 class ViewController: UIViewController, ArrayLabels {
     
-    var myLabel = GenerationLabel()
-
+    var myLabels = [GenerationLabel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        myLabel.center = self.view.center
-        self.view.addSubview(myLabel)
-        
-        arrLabels().forEach {
+        myLabels = arrLabels()
+        myLabels.forEach {
             self.view.addSubview($0)
         }
         
+        var dictionary = [String: Any]()
         
+        for i in 0..<myLabels.count {
+            dictionary["label\(i)"] = myLabels[i]
+            myLabels[i].translatesAutoresizingMaskIntoConstraints = false
+        }
         
+        self.view.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-50-[label0]-10-[label1]-10-[label2]-10-[label3]-10-[label4]",
+            options: [],
+            metrics: nil,
+            views: dictionary))
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-[label0]-10-[label1]-10-[label2]-10-[label3]-10-[label4]",
+            options: [],
+            metrics: nil,
+            views: dictionary))
     }
-
-    
-
 }
 
