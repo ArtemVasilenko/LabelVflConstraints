1e18d959e71293cc3f2753e76283967f96fc8bb6
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-15 20:09:08 +0300

    Initial Commit

---
+++ LabelVflConstraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabelVflConstraints
+//
+//  Created by Артем on 5/15/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
